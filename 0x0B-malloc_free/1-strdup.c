2de875c2a46089fdf8a416651b9fe9e6b70cfc84
Adeyemomyko <adeyemomichael68@gmail.com>
2023-08-16 07:45:44 -0700

    hyper

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
-		
 #include <stdlib.h>
-		
-#include "main.h"
-		
+#include "main.h"		
 /**
-		
- * _strdup - duplicate to new memory space location
-		
- * @str: char
-		
- * Return: 0
-		
+ * _strdup - duplicate to new memory space location	
+ * @str: char	
+ * Return: 0		
  */
-		
 char *_strdup(char *str)
 		
 {
 		
 	char *aaa;
-		
-	int i, r = 0;
-		
 
-		
+	int i, r = 0;
+	
 	if (str == NULL)
-		
-		return (NULL);
+	
+	return (NULL);
 		
 	i = 0;
 		
@@ -34,24 +24,13 @@ char *_strdup(char *str)
 		
 		i++;
 		
-
-		
 	aaa = malloc(sizeof(char) * (i + 1));
-		
-
 		
 	if (aaa == NULL)
 		
 		return (NULL);
 		
-
-		
-	for (r = 0; str[r]; r++)
-		
-		aaa[r] = str[r];
-		
-
-		
-	return (aaa);
-		
+	for (r = 0; str[r]; r++)	
+	aaa[r] = str[r];
+	return (aaa);	
 }
